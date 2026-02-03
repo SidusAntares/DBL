0b9c3edcd0fb8290619699ff742c75fc49e27f5d
SidusAntares <2934978005@qq.com>
2026-02-04 00:06:28 +0800

    timematch数据加载基本完成

--- train.py
+++ train.py
@@ -7,15 +7,14 @@ import argparse
 import json
 import os
 import pprint
+import random
 import time
 import pickle as pkl
 
-import numpy as np
-import torch
+
 import torch.nn as nn
-import torch.utils.data as data
 import torchnet as tnt
-from Function.CF_Dataset import CFDataset
+
 from Function.metrics import confusion_matrix_analysis
 from Function.miou import IoU
 from Function import utils
@@ -23,14 +22,46 @@ from Function.weight_init import weight_init
 import load_model
 
 from dataset import PixelSetData, create_evaluation_loaders, create_train_loader
+from utils import label_utils
+from torch.utils.data.sampler import WeightedRandomSampler
+from collections import Counter
+
+
+import numpy as np
+import torch
+from torch.utils import data
+from torchvision import transforms
+
+from dataset import PixelSetData
+from transforms import (
+    Normalize,
+    RandomSamplePixels,
+    RandomSampleTimeSteps,
+    ToTensor,
+)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--model",
-    default="JM",
-    type=str,
-    help="Which dataset to use. Can be one of: (JM/CF/PAS_SS)",
-)
+
+
+# 数据路径与域
+parser.add_argument('--data_root', default='/mnt/d/All_Documents/documents/ViT/dataset/timematcha', type=str,
+                    help='Path to datasets root directory')
+parser.add_argument('--source', default='denmark/32VNH/2017', type=str)
+parser.add_argument('--target', default='france/30TXT/2017', type=str)
+
+# 类别处理
+parser.add_argument('--combine_spring_and_winter', action='store_true')
+
+# 数据划分
+parser.add_argument('--num_folds', default=3, type=int)
+parser.add_argument("--val_ratio", default=0.1, type=float)
+parser.add_argument("--test_ratio", default=0.2, type=float)
+
+
+# 评估
+parser.add_argument('--sample_pixels_val', action='store_true') # 布尔型开关参数（flag），它不需要传值，只需在命令行中出现或不出现该选项
+
 
 ## Hyperparameters of DBL
 # parser.add_argument("--encoder_widths", default="[64,64,64,128]", type=str)
@@ -198,9 +229,7 @@ def save_results(metrics, conf_mat, config):
         json.dump(metrics, outfile, indent=4)
     pkl.dump(
         conf_mat,
-        open(
-            os.path.join(config.res_dir, "conf_mat.pkl"), "wb"
-        ),
+        open(os.path.join(config.res_dir, "conf_mat.pkl"), "wb"),
     )
 
 def overall_performance(config):
@@ -223,54 +252,110 @@ def overall_performance(config):
     with open(os.path.join(config.res_dir, "overall.json"), "w") as file:
         file.write(json.dumps(perf_converted, indent=4))
 
-def main(config):
+class TupleDataset(data.Dataset):
+    def __init__(self, dataset1, dataset2):
+        super().__init__()
+        self.weak = dataset1
+        self.strong = dataset2
+        assert len(dataset1) == len(dataset2)
+        self.len = len(dataset1)
 
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        return (self.weak[index], self.strong[index])
+
+def get_data_loaders(splits, config, balance_source=True):
+
+    strong_aug = transforms.Compose([
+            RandomSamplePixels(config.num_pixels),
+            RandomSampleTimeSteps(config.seq_length),
+            Normalize(),
+            ToTensor(),
+    ])
+
+    source_dataset = PixelSetData(config.data_root, config.source,
+            config.classes, strong_aug,
+            indices=splits[config.source]['train'],)
+
+    if balance_source:
+        source_labels = source_dataset.get_labels()
+        freq = Counter(source_labels)
+        class_weight = {x: 1.0 / freq[x] for x in freq}
+        source_weights = [class_weight[x] for x in source_labels]
+        sampler = WeightedRandomSampler(source_weights, len(source_labels))
+        print("using balanced loader for source")
+        source_loader = data.DataLoader(
+            source_dataset,
+            num_workers=config.num_workers,
+            pin_memory=True,
+            sampler=sampler,
+            batch_size=config.batch_size,
+            drop_last=True,
+        )
+    else:
+        source_loader = data.DataLoader(
+            source_dataset,
+            num_workers=config.num_workers,
+            pin_memory=True,
+            batch_size=config.batch_size,
+            shuffle=True,
+            drop_last=True,
+        )
+    print(f'size of source dataset: {len(source_dataset)} ({len(source_loader)} batches)')
+
+    return source_loader
+
+def create_train_val_test_folds(datasets, num_folds, num_indices, val_ratio=0.1, test_ratio=0.2):
+    folds = []
+    for _ in range(num_folds):
+        splits = {}
+        for dataset in datasets:
+            if type(num_indices) == dict:
+                indices = list(range(num_indices[dataset]))
+            else:
+                indices = list(range(num_indices))
+            n = len(indices)
+            n_test = int(test_ratio * n)
+            n_val = int(val_ratio * n)
+            n_train = n - n_test - n_val
+
+            random.shuffle(indices)
+
+            train_indices = set(indices[:n_train])
+            val_indices = set(indices[n_train:n_train + n_val])
+            test_indices = set(indices[-n_test:])
+            assert set.intersection(train_indices, val_indices, test_indices) == set()
+            assert len(train_indices) + len(val_indices) + len(test_indices) == n
+
+            splits[dataset] = {'train': train_indices, 'val': val_indices, 'test': test_indices}
+        folds.append(splits)
+    return folds
+
+
+def main(config):
+    random.seed(config.seed)
+    np.random.seed(config.seed)
+    torch.manual_seed(config.seed)
     prepare_output(config)
     device = torch.device(config.device)
-    if config.model == "JM":
-        dt_train = CFDataset(data_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_data/Jingmen_Train',
-                         label_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_label/Label_Train',
-                         norm=True) #dir to the JM train set
-        dt_val = CFDataset(data_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_data/Jingmen_Val',
-                       label_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_label/Label_Val', norm=True) #dir to the JM val set
-        dt_test = CFDataset(data_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_data/Jingmen_Test',
-                        label_dir='/mnt/d/All_Documents/documents/JMDataset/JMDataset/Jingmen_label/Label_Test', norm=True) #dir to the JM test set
-    elif config.model == "CF":
-        dt_train = CFDataset(data_dir='CFDATA/mydata/CF_Train',
-                          label_dir='CFlabel/mylabel/Label_Train',
-                          norm=True) #dir to the CF train set
-        dt_val = CFDataset(data_dir='CFDATA/mydata/CF_Val',
-                        label_dir='CFlabel/mylabel/Label_Val', norm=True) #dir to the CF val set
-        dt_test = CFDataset(data_dir='CFDATA/mydata/CF_Test',
-                        label_dir='CFlabel/mylabel/Label_Test', norm=True) #dir to the CF test set
 
-    collate_fn = lambda x: utils.pad_collate(x, pad_value=config.pad_value)
+    cfg = config
+    source_classes = label_utils.get_classes(cfg.source.split('/')[0],
+                                             combine_spring_and_winter=cfg.combine_spring_and_winter)
+    source_data = PixelSetData(cfg.data_root, cfg.source, source_classes)
+    labels, counts = np.unique(source_data.get_labels(), return_counts=True)
+    source_classes = [source_classes[i] for i in labels[counts >= 200]]
+    print('Using classes:', source_classes)
+    cfg.classes = source_classes
+    cfg.num_classes = len(source_classes)
+    # Randomly assign parcels to train/val/test
+    indices = {config.source: len(source_data),
+               config.target: len(PixelSetData(config.data_root, config.target, source_classes))}
+    folds = create_train_val_test_folds([config.source, config.target], config.num_folds, indices, config.val_ratio,
+                                        config.test_ratio)
 
-    train_loader = data.DataLoader(
-        dt_train,
-        batch_size=config.batch_size,
-        shuffle=True,
-        drop_last=True,
-        collate_fn=collate_fn
-    )
-    val_loader = data.DataLoader(
-        dt_val,
-        batch_size=config.batch_size,
-        shuffle=True,
-        drop_last=True,
-        collate_fn=collate_fn
-    )
-    test_loader = data.DataLoader(
-        dt_test,
-        batch_size=config.batch_size,
-        shuffle=True,
-        drop_last=True,
-        collate_fn=collate_fn
-    )
-
-    print(
-        "Train {}, Val {}, Test {}".format(len(dt_train), len(dt_val), len(dt_test))
-    )
     #load DBL model
     model = load_model.get_model(config, mode="semantic")
     config.N_params = utils.get_ntrainparams(model)
@@ -298,90 +383,98 @@ def main(config):
     criterion = nn.CrossEntropyLoss(weight=weights)
     trainlog = {}
     best_mIoU = 0
-    for epoch in range(1, config.epochs + 1):
-        print("EPOCH {}/{}".format(epoch, config.epochs))
+    for fold_num, splits in enumerate(folds):
+        print(f'Starting fold {fold_num}...')
 
-        model.train()
-        train_metrics = iterate(
-            model,
-            data_loader=train_loader,
-            criterion=criterion,
-            config=config,
-            optimizer=optimizer,
-            mode="train",
-            device=device,
-            # epoch=epoch,
-        )
-        if epoch % config.val_every == 0 and epoch > config.val_after:
-            print("Validation . . . ")
-            model.eval()
-            val_metrics = iterate(
+        config.fold_num = fold_num
+
+        sample_pixels_val = config.sample_pixels_val
+        val_loader, test_loader = create_evaluation_loaders(config.target, splits, config, sample_pixels_val)
+        source_loader = get_data_loaders(splits, config, config.balance_source)
+        for epoch in range(1, config.epochs + 1):
+            print("EPOCH {}/{}".format(epoch, config.epochs))
+
+            model.train()
+            train_metrics = iterate(
                 model,
-                data_loader=val_loader,
+                data_loader=source_loader,
                 criterion=criterion,
                 config=config,
                 optimizer=optimizer,
-                mode="val",
+                mode="train",
                 device=device,
                 # epoch=epoch,
             )
-
-            print(
-                "Loss {:.4f},  Acc {:.2f},  IoU {:.4f}".format(
-                    val_metrics["val_loss"],
-                    val_metrics["val_accuracy"],
-                    val_metrics["val_IoU"],
+            if epoch % config.val_every == 0 and epoch > config.val_after:
+                print("Validation . . . ")
+                model.eval()
+                val_metrics = iterate(
+                    model,
+                    data_loader=val_loader,
+                    criterion=criterion,
+                    config=config,
+                    optimizer=optimizer,
+                    mode="val",
+                    device=device,
+                    # epoch=epoch,
                 )
-            )
 
-            trainlog[epoch] = {**train_metrics, **val_metrics}
-            checkpoint(trainlog, config)
-            if val_metrics["val_IoU"] >= best_mIoU:
-                best_mIoU = val_metrics["val_IoU"]
-                torch.save(
-                    {
-                        "epoch": epoch,
-                        "state_dict": model.state_dict(),
-                        "optimizer": optimizer.state_dict(),
-                    },
-                    os.path.join(
-                        config.res_dir,  "model.pth.tar"
-                    ),
+                print(
+                    "Loss {:.4f},  Acc {:.2f},  IoU {:.4f}".format(
+                        val_metrics["val_loss"],
+                        val_metrics["val_accuracy"],
+                        val_metrics["val_IoU"],
+                    )
                 )
-        else:
-            trainlog[epoch] = {**train_metrics}
-            checkpoint(trainlog, config)
 
-    print("Testing best epoch . . .")
-    model.load_state_dict(
-        torch.load(
-            os.path.join(
-                config.res_dir, "model.pth.tar"
-            )
-        )["state_dict"]
-    )
-    model.eval()
+                trainlog[epoch] = {**train_metrics, **val_metrics}
+                checkpoint(trainlog, config)
+                if val_metrics["val_IoU"] >= best_mIoU:
+                    best_mIoU = val_metrics["val_IoU"]
+                    torch.save(
+                        {
+                            "epoch": epoch,
+                            "state_dict": model.state_dict(),
+                            "optimizer": optimizer.state_dict(),
+                        },
+                        os.path.join(
+                            config.res_dir, "model.pth.tar"
+                        ),
+                    )
+            else:
+                trainlog[epoch] = {**train_metrics}
+                checkpoint(trainlog, config)
 
-    test_metrics, conf_mat = iterate(
-        model,
-        data_loader=test_loader,
-        criterion=criterion,
-        config=config,
-        optimizer=optimizer,
-        mode="test",
-        device=device,
-        # epoch=epoch,
-    )
-    print(
-        "Loss {:.4f},  Acc {:.2f},  IoU {:.4f}".format(
-            test_metrics["test_loss"],
-            test_metrics["test_accuracy"],
-            test_metrics["test_IoU"],
+        print("Testing best epoch . . .")
+        model.load_state_dict(
+            torch.load(
+                os.path.join(
+                    config.res_dir, "model.pth.tar"
+                )
+            )["state_dict"]
         )
-    )
-    save_results(test_metrics, conf_mat.cpu().numpy(), config)
+        model.eval()
 
-    overall_performance(config)
+        test_metrics, conf_mat = iterate(
+            model,
+            data_loader=test_loader,
+            criterion=criterion,
+            config=config,
+            optimizer=optimizer,
+            mode="test",
+            device=device,
+            # epoch=epoch,
+        )
+        print(
+            "Loss {:.4f},  Acc {:.2f},  IoU {:.4f}".format(
+                test_metrics["test_loss"],
+                test_metrics["test_accuracy"],
+                test_metrics["test_IoU"],
+            )
+        )
+        save_results(test_metrics, conf_mat.cpu().numpy(), config)
+
+        overall_performance(config)
 
 if __name__ == "__main__":
     config = parser.parse_args()
